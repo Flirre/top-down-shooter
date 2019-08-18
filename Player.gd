1415a76efe2d2365f7cc399a7ed36dda9a071ebe
Filip Lindahl <filip.g.lindahl@gmail.com>
2019-08-18 21:35:58 +0200

    player bound inside map

--- Player.gd
+++ Player.gd
@@ -14,28 +14,39 @@ func _process(delta):
 	if Input.is_action_pressed("fire"):
 		fire()
 	reloading -= delta
-		
+
 func fire():
 	if readyToFire():
 		var left_laser = LASER.instance()
 		var right_laser = LASER.instance()
-		
+
 		left_laser.global_position = global_position
 		left_laser.position.x -= 34
 		left_laser.position.y -= 27
-		
+
 		right_laser.global_position = global_position
 		right_laser.position.x += 35
 		right_laser.position.y -= 27
-		
+
 		get_parent().add_child(left_laser)
 		get_parent().add_child(right_laser)
 		reloading = RELOAD_TIME
-	
+
 func readyToFire():
 	return (reloading <= 0.0)
 
+
+# warning-ignore:unused_argument
 func _physics_process(delta):
+	move()
+
+func move():
+	var movement: Vector2 = get_movement_input()
+	var normalized_movement = movement.normalized()
+	# warning-ignore:return_value_discarded
+	move_and_slide(normalized_movement * PLAYER_SPEED, Vector2(0,0))
+
+func get_movement_input():
 	var move_vec = Vector2()
 	if Input.is_action_pressed("move_up"):
 		move_vec.y = -1
@@ -45,5 +56,4 @@ func _physics_process(delta):
 		move_vec.x = -1
 	if Input.is_action_pressed("move_right"):
 		move_vec.x = 1
-	move_vec = move_vec.normalized()
-	move_and_collide(move_vec * PLAYER_SPEED * delta) 	
+	return move_vec
