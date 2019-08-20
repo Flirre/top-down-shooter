91b0e754547b7332004fc74037343e19b3499356
Filip Lindahl <filip.g.lindahl@gmail.com>
2019-08-20 21:30:11 +0200

    damage, knockback, refactoring

--- Bullet.gd
+++ Bullet.gd
@@ -5,6 +5,7 @@ extends Area2D
 # var b = "text"
 const LASER_VELOCITY: Vector2 = Vector2(0, -400)
 var health: int = 10
+const DAMAGE: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,6 +23,7 @@ func removeWhenOffScreen():
 	if global_position.y < 0:
 		queue_free()
 
-
 func _on_Bullet_body_entered(body):
-	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage(DAMAGE)
+		queue_free()
--- Player.gd
+++ Player.gd
@@ -1,20 +1,36 @@
-extends KinematicBody2D
+extends Entity
 
 const LASER = preload("res://Laser.tscn")
-const PLAYER_SPEED: int = 200
-const RELOAD_TIME: float = 0.5
-var reloading: float = 0.0
 
-# Called when the node enters the scene tree for the first time.
+# Sets the constants from the Entity class when readying
 func _ready():
-	pass # Replace with function body.
+	SPEED = 200
+	RELOAD_TIME = 0.5
+	DAMAGE = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("fire"):
-		fire()
 	reloading -= delta
 
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	input_loop()
+	movement_loop()
+	damage_loop(delta)
+
+func input_loop():
+	var LEFT 	= Input.is_action_pressed("move_left")
+	var UP 		= Input.is_action_pressed("move_up")
+	var RIGHT 	= Input.is_action_pressed("move_right")
+	var DOWN 	= Input.is_action_pressed("move_down")
+	var FIRE 	= Input.is_action_pressed("fire")
+	
+	movement_direction.x = -int(LEFT) + int(RIGHT)
+	movement_direction.y = -int(UP) + int(DOWN)
+	
+	if (FIRE):
+		fire()
+		
 func fire():
 	if readyToFire():
 		var left_laser = LASER.instance()
@@ -31,29 +47,3 @@ func fire():
 		get_parent().add_child(left_laser)
 		get_parent().add_child(right_laser)
 		reloading = RELOAD_TIME
-
-func readyToFire():
-	return (reloading <= 0.0)
-
-
-# warning-ignore:unused_argument
-func _physics_process(delta):
-	move()
-
-func move():
-	var movement: Vector2 = get_movement_input()
-	var normalized_movement = movement.normalized()
-	# warning-ignore:return_value_discarded
-	move_and_slide(normalized_movement * PLAYER_SPEED, Vector2(0,0))
-
-func get_movement_input():
-	var move_vec = Vector2()
-	if Input.is_action_pressed("move_up"):
-		move_vec.y = -1
-	if Input.is_action_pressed("move_down"):
-		move_vec.y = 1
-	if Input.is_action_pressed("move_left"):
-		move_vec.x = -1
-	if Input.is_action_pressed("move_right"):
-		move_vec.x = 1
-	return move_vec
